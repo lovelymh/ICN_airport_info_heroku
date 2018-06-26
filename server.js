c81ff64958cf23be826a6aacbe461cf3a77b9bc2
mihee <sadrainbow26@naver.com>
2018-06-26 18:25:34 +0900

    Initial commit2s

--- server.js
+++ server.js
@@ -202,6 +202,11 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
+app.post('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+
 app.get('/api/hello', (req, res) => {
 
   var timer = setTimeout(function(){
